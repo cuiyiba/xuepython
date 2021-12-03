9a0641ce909ee67198fc391b661004f6e403d4c5
cuiyiba <52354578+cuiyiba@users.noreply.github.com>
2021-12-03 17:04:40 +0800

    Add files via upload
    单表sql联系

---
+++ 单表查询练习.sql
@@ -0,0 +1,149 @@
+/*
+company公司数据库：
+	|--dept:部门表
+		|--`deptno` 部门编号
+		|--`dname`  部门名称
+		|--`loc`    部门所在地址
+  
+
+	|--employees:员工表
+		|--`empno`	员工编号
+		|--`ename`	员工姓名
+		|--`job`	工作
+		|--`MGR`	上级领导
+		|--`hiredate`   入职日期
+		|--`sal`	薪资
+		|--`comm`       将金
+		|--`deptno`     部门编号
+
+/*
+	1. 查询出部门编号为30的所有员工
+	ELECT
+	*
+FROM
+	t_employees
+WHERE
+	deptno = 30
+	2. 所有经理的姓名、编号和部门编号。
+SELECT
+	*
+FROM
+	t_employees
+WHERE
+	job='经理'
+	3. 找出奖金高于工资的员工。
+SELECT
+	*
+FROM
+	t_employees
+WHERE
+	comm>sal
+	4. 找出奖金高于工资60%的员工。
+	SELECT
+	*
+FROM
+	t_employees
+WHERE
+	comm>sal%60
+	5. 找出部门编号为10中所有经理，和部门编号为20中所有分析员的详细资料。
+	SELECT
+	*
+FROM
+	t_employees
+WHERE
+	deptno = 10
+AND job = '经理'
+UNION
+	SELECT
+		*
+	FROM
+		t_employees
+	WHERE
+		deptno = 20
+	AND job = '分析员'
+	6. 找出部门编号为10中所有经理，部门编号为20中所有分析员，还有即不是经理又不是武装上将但其工资大或等于3000的所有员工详细资料。
+	SELECT
+	*
+FROM
+	t_employees
+WHERE
+	(job = "经理" AND deptno = 10)
+OR (
+	job = "分析员"
+	AND deptno = 20
+)
+OR (
+	job NOT IN ("经理", "武装上将")
+)
+AND sal >= 3000
+	7. 无奖金或奖金低于1000的员工。
+	SELECT
+	*
+
+FROM
+	t_employees
+WHERE
+comm<=1000
+	8. 查询名字由三个字组成的员工。
+	SELECT
+	*
+
+FROM
+	t_employees
+WHERE
+ename LIKE'___'
+
+
+	9. 查询2000年以及以后入职的员工。
+	SELECT
+	*
+
+FROM
+	t_employees
+WHERE
+hiredate>='2000-1-01'
+	10. 查询所有员工详细信息，用编号升序排序
+	SELECT
+	*
+FROM
+	t_employees
+
+ORDER BY
+	empno
+
+	11. 查询所有员工详细信息，用工资降序排序，如果工资相同使用入职日期升序排序
+	SELECT
+	*
+FROM
+	t_employees
+ORDER BY
+	sal DESC,
+	hiredate ASC
+	12. 查询每个部门的平均工资
+	SELECT
+	deptno,
+	avg(sal)
+FROM
+	t_employees
+GROUP BY
+	deptno
+	13. 查询每个部门的雇员数量。
+	SELECT
+	deptno,
+	COUNT(*)
+FROM
+	t_employees
+GROUP BY
+	deptno
+	14. 查询每种工作的最高工资、最低工资、人数
+	SELECT
+	job,
+	MAX(sal),
+	MIN(sal),
+COUNT(*)
+FROM
+	t_employees
+GROUP BY
+	job
+*/
+
